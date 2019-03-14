768adb2ba2857e72a26069d1627c9fdcfaa175e3
Roman_Shramkov <roman_shramkov@epam.com>
2019-03-14 18:52:28 +0200

    Initial commit of the app code

---
+++ src/main/java/io/romans/samplespringapp/DemoSpringAppApplication.java
@@ -0,0 +1,13 @@
+package io.romans.samplespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAppApplication.class, args);
+	}
+
+}
